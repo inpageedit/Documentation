a84d5bf67aebdefe2c5732dfff0334ac1d1f923e
机智的小鱼君 <dragon-fish@qq.com>
2021-08-13 20:09:27 +0800

    update: + 14.1.9

--- src/.vuepress/components/issues.vue
+++ src/.vuepress/components/issues.vue
@@ -1,5 +1,5 @@
 <template>
-<a class="issues-link external" :id="'issues-' + id" :href="'https://github.com/Dragon-Fish/InPageEdit-v2/issues/' + id">
+<a class="issues-link external" :id="'issues-' + id" :href="'https://github.com/inpageedit/inpageedit-v2/issues/' + id">
   <slot>issues#{{ id }}</slot>
   <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15" class="icon outbound">
     <path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path>
