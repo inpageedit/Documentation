842c27d3ab0c5d0e735e58667b847bdd6a937043
机智的小鱼君 <dragon-fish@qq.com>
2022-03-16 16:51:38 +0800

    doc: update 14.3.0

--- src/.vuepress/components/install.js
+++ src/.vuepress/components/install.js
@@ -1,1 +1,7 @@
-mw.loader.load('https://cdn.jsdelivr.net/npm/mediawiki-inpageedit@latest/dist/InPageEdit.min.js');
+/**
+ * 从下面的方法中任选一个即可 Choose one from the following
+ */
+// 1. jsdelivr
+mw.loader.load('https://cdn.jsdelivr.net/npm/mediawiki-inpageedit')
+// 2. unpkg
+mw.loader.load('https://unpkg.com/mediawiki-inpageedit')
